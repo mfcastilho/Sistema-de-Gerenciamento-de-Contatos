1957c2785aed9b0994176b1725a989eb5450795c
mfcastilho <fredericocastilho@hotmail.com>
2023-03-28 19:50:39 -0300

    feat: creating users migration

--- backend/src/database/migrations/20230328222954-create_contacts_table.js
+++ backend/src/database/migrations/20230328222954-create_contacts_table.js
@@ -3,20 +3,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
+    await queryInterface.createTable('contacts', { 
+      
+    });
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+    await queryInterface.dropTable('contacts');
   }
 };
--- backend/src/database/migrations/20230328223252-create_users_table.js
+++ backend/src/database/migrations/20230328223252-create_users_table.js
@@ -3,20 +3,36 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
+    await queryInterface.createTable('users', {
+      id:{
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false
+      },
+      name:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password:{
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    },
+    {
+      tableName:"db_contact_management",
+      timestamps: true
+    });
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+    await queryInterface.dropTable('users');
+
   }
 };
