a957b09d50aadaf668cb712195361eab4e552614
mfcastilho <fredericocastilho@hotmail.com>
2023-03-28 23:40:56 -0300

    users and contacts route and controller structures

---
+++ backend/src/database/migrations/20230328222954-create_contacts_table.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.createTable('contacts', { 
+      id:{
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false
+      },
+      name:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      tel:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      userId:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        references:{
+          model: "users",
+          key: "id"
+        }
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false 
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+
+    });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.dropTable('contacts');
+  }
+};
