af015cbfe584605f644d88536a46d2d2faf2de66
mfcastilho <fredericocastilho@hotmail.com>
2023-03-30 21:20:26 -0300

    implementing deleteContact method in the ContactController

--- backend/src/controllers/ContactController.js
+++ backend/src/controllers/ContactController.js
@@ -155,8 +155,37 @@ const ContactController = {
           }
      },
 
-     deleteContact: (req, res)=>{
+     deleteContact: async (req, res)=>{
+          try {
 
+               const {id} = req.params;
+               const contact = await Contact.findByPk(id);
+
+               if(!contact){
+                    return res.status(404).json({error: true, message: "Contato não encontrado"});
+               }
+
+               const contactId = await Contact.destroy({
+                    where:{id}
+               });
+
+               return res.status(200).json({message: `Contato de id:${contactId} deletado com sucesso.`});
+
+
+          } catch (error) {
+               
+               if (error.name === "SequelizeConnectionRefusedError"){
+                    return res.status(500).json({error: true, message: "Sistema indisponível, tente novamente mais tarde!"})
+               }
+            
+               if (error.name === "SequelizeUniqueConstraintError"){
+                    return res.status(400).json(error.parent.sqlMessage);
+               }
+            
+               if (error.name === "SequelizeValidationError"){
+                    return res.status(400).json({error: true, message: `${error.errors[0].type} at ${error.errors[0].path}`})
+               }
+          }
      }
 }
 
