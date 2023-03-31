cdec23479402709ab34d006680a87d3f12db36bb
mfcastilho <fredericocastilho@hotmail.com>
2023-03-31 03:58:53 -0300

    styling the login and register buttons

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -8,10 +8,10 @@ export function Header(props) {
      return(
           <header>
                <div className="logo-box">
-                    <a href=""><h1 className="logo">Contacts</h1></a>                   
+                    <a href="/"><h1 className="logo">Contacts</h1></a>                   
                </div>
                <div className="profile">
-                    <a href=""><img src=".\src\assets\profile.png" alt="profile-image" /></a>    
+                    <a href="/"><img src=".\src\assets\profile.png" alt="profile-image" /></a>    
                </div>
           </header>
      )
