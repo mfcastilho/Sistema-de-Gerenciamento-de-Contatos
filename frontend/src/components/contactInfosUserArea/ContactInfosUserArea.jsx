63f8e8b7e8f688f863783e4b6e8c1b5383a932c9
mfcastilho <fredericocastilho@hotmail.com>
2023-04-02 19:13:38 -0300

    ContactInfosUserArea function finalized

--- frontend/src/components/contactInfosUserArea/ContactInfosUserArea.jsx
+++ frontend/src/components/contactInfosUserArea/ContactInfosUserArea.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import "./ContactInfosUserArea.css";
 
 
 
 function ContactInfosUserArea(){
+
+     const user = JSON.parse(localStorage.getItem("user"));
+
+     const location = useLocation();
+     console.log(location.state.contact);
+     const contact = location.state.contact;
      return(
-          <div>
-               <h2>Contact Infos</h2>
+          <div className="user-contact-container">
+               <h2>Informações do contato da {user.name}</h2>
+               <div className="contac-infos-box">
+                    <div><strong>Nome: </strong><span>{contact.name}</span></div>
+                    <div><strong>Telefone: </strong><span>{contact.tel}</span></div>
+                    <div><strong>Email: </strong><span>{contact.email}</span></div>     
+               </div>
           </div>
      )
 }
